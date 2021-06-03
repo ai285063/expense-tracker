3f789fdd1bdcad9e1069df9dd690df0b4dfd4563
Meg <ai285063@gmail.com>
2021-06-03 16:48:32 +0800

    feat: add views main/index

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 require('./config/mongoose')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('<h1>Expense Tracker</h1>')
+  res.render('index')
 })
 
 app.listen(port, () => {
