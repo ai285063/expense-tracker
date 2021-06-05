e457b0a48b9f234b73725743b25911dc863124b1
Meg <ai285063@gmail.com>
2021-06-05 20:10:54 +0800

    feat: add date filter function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,16 +6,21 @@ const Record = require('../../models/record')
 
 router.get('/', async (req, res) => {
   const categoryChosen = req.query.category
+  const monthChosen = req.query.month
   try {
     let totalAmount = 0
-    const [categories, records] = await Promise.all([Category.find().lean(), Record.find(filter(categoryChosen)).lean()])
+    const [categories, records] = await Promise.all([Category.find().lean(),
+      Record.aggregate([
+        { $match: { $and: [getCategory(categoryChosen), getMonth(monthChosen)] } }
+      ])
+    ])
     const dates = []
     for (let i = 0; i < records.length; i++) {
       dates[i] = new Date(records[i].date).toISOString().substring(0, 10)
       records[i].date = dates[i]
       totalAmount += records[i].amount
     }
-    return res.render('index', { categories, records, totalAmount, categoryChosen })
+    return res.render('index', { categories, records, totalAmount, categoryChosen, monthChosen })
   } catch (err) {
     console.log(err)
   }
@@ -23,13 +28,23 @@ router.get('/', async (req, res) => {
 
 module.exports = router
 
-function filter (categoryChosen) {
+function getCategory (categoryChosen) {
   if (categoryChosen) {
     if (categoryChosen !== '所有類別') {
       return { category: categoryChosen }
     } else if (categoryChosen === '所有類別') {
       return {}
     }
+  } else {
+    return {}
+  }
+}
+
+function getMonth (monthChosen) {
+  if (monthChosen) {
+    const firstDayOfMonth = new Date(monthChosen)
+    const LastDayOfMonth = new Date(monthChosen.substring(0, 4), monthChosen.substring(5, 7), 0)
+    return { date: { $gt: firstDayOfMonth, $lt: LastDayOfMonth } }
   } else {
     return {}
   }
