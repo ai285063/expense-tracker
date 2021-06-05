bc205c10c492672f02da9216b7e3333a1abfc2f5
Meg <ai285063@gmail.com>
2021-06-05 12:17:24 +0800

    feat: modify views index

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,21 +4,13 @@ const router = express.Router()
 const Category = require('../../models/category')
 const Record = require('../../models/record')
 
-router.get('/', (req, res) => {
-  // const categoryList = Category.find()
-  // const categories = []
-  // categoryList.exec((err, data) => {
-  // })
-  const totalAmount = 135
-  Category
-    .find()
-    .lean()
-    .then(results => res.render('index', { categories: results }))
-    .catch(err => console.log(err))
-  Record.find()
-    .lean()
-    .then(records => res.render('index', { records, totalAmount }))
-    .catch(err => console.error(err))
+router.get('/', async (req, res) => {
+  try {
+    const [categories, records] = await Promise.all([Category.find().lean(), Record.find().lean()])
+    return res.render('index', { categories, records })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
